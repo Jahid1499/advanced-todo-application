bd63c2bb9111c9c0e876d4c7d7cbc1e48ddf4794
jahid <jahid15-1499@diu.edu.bd>
2024-05-20 15:45:12 +0600

    Feat: templating done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-function App() {
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
+
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-green-800 py-12 px-5 text-white">
-        Hello world!
-      </h1>
+      <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+        <Navbar />
+
+        <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+          <Header />
+          <hr className="mt-4" />
+          <TodoList />
+          <hr className="mt-4" />
+          <Footer />
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default App;
