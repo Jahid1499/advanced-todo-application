6d5295d420b790c7c556fcb637bcabeb33256688
jahid <jahid15-1499@diu.edu.bd>
2024-05-20 15:18:51 +0600

    feat: todos api done

--- src/features/todos/todosApi.ts
+++ src/features/todos/todosApi.ts
@@ -2,10 +2,30 @@ import { apiSlice } from "../api/apiSlice";
 
 export const todosApi = apiSlice.injectEndpoints({
     endpoints: builder => ({
-        getTodo: builder.query({
+        getTodos: builder.query({
             query: () => "/todos"
-        })
-
-        // end points hear
+        }),
+        addTodos: builder.mutation({
+            query: (data) => ({
+                url: '/todos',
+                method: 'POST',
+                body: data
+            })
+        }),
+        updateTodos: builder.mutation({
+            query: ({ id, data }) => ({
+                url: `/todos/${id}`,
+                method: 'PATCH',
+                body: data
+            })
+        }),
+        deleteTodos: builder.mutation({
+            query: (id) => ({
+                url: `/todos/${id}`,
+                method: 'DELETE',
+            })
+        }),
     })
 })
+
+export const { useGetTodosQuery, useAddTodosMutation, useUpdateTodosMutation, useDeleteTodosMutation } = todosApi;
