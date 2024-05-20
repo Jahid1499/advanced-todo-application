8c951d1864ac3608cae8080a44ffb49e505c5a34
jahid <jahid15-1499@diu.edu.bd>
2024-05-20 22:52:46 +0600

    Feat: display todo list corosponding todo status and color status

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { useGetFiltersQuery } from "../features/filters/filterApi";
 import { useGetTodosQuery } from "../features/todos/todosApi";
 import Todo from "./Todo";
 import Error from "./ui/Error";
@@ -12,6 +13,16 @@ type TodoTypes = {
 const TodoList = () => {
   const { data: todos, isLoading, isError } = useGetTodosQuery({});
 
+  const { data: filters } = useGetFiltersQuery({});
+
+  let status = "";
+  let colors = [];
+
+  if (filters && filters.length > 0) {
+    status = filters[0]?.status;
+    colors = filters[0]?.colors;
+  }
+
   let content = null;
 
   if (isLoading) {
@@ -25,11 +36,31 @@ const TodoList = () => {
   if (!isLoading && !isError && todos?.length === 0) {
     content = <Error message="No todos found!" />;
   }
+  const filterByStatus = (todo: TodoTypes) => {
+    switch (status) {
+      case "Complete":
+        return todo.completed;
+
+      case "Incomplete":
+        return !todo.completed;
+
+      default:
+        return true;
+    }
+  };
+
+  const filterByColors = (todo: TodoTypes) => {
+    if (colors?.length > 0) {
+      return colors.includes(todo?.color);
+    }
+    return true;
+  };
 
   if (!isLoading && !isError && todos?.length > 0) {
-    content = todos.map((todo: TodoTypes) => (
-      <Todo key={todo.id} todo={todo} />
-    ));
+    content = todos
+      .filter(filterByStatus)
+      .filter(filterByColors)
+      .map((todo: TodoTypes) => <Todo key={todo.id} todo={todo} />);
   }
 
   return <>{content}</>;
