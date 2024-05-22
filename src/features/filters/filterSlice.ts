8c992dc8e6e14b237ff5ac64f6e032a9fc94092e
jahid <jahid15-1499@diu.edu.bd>
2024-05-22 14:33:20 +0600

    clean todosslice and filtersslice

--- src/features/filters/filterSlice.ts
+++ src/features/filters/filterSlice.ts
@@ -1,20 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    accessToken: undefined,
-    user: undefined,
+
 }
 
 const filterSlice = createSlice({
     name: 'filter',
     initialState,
     reducers: {
-        userLoggedIn: (state, action) => {
-            state.accessToken = action.payload.accessToken;
-            state.user = action.payload.user;
-        },
+
     }
 })
 
-export const { userLoggedIn } = filterSlice.actions;
+export const { } = filterSlice.actions;
 export default filterSlice.reducer;
--- src/features/todos/todosSlice.ts
+++ src/features/todos/todosSlice.ts
@@ -1,30 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-type TodosState = {
-    text: string,
-    completed: boolean,
-    id: number,
-    color: string,
-}[]
 
-const initialState: TodosState = [
-    {
-        text: "My todo",
-        completed: true,
-        id: 1000,
-        color: "red",
-    }
-]
+const initialState = {}
 
 const todosSlice = createSlice({
     name: 'todos',
     initialState,
     reducers: {
-        userLoggedIn: (state, action) => {
-            state[0].text = action.payload.text
-        },
+
     }
 })
 
-export const { userLoggedIn } = todosSlice.actions;
+export const { } = todosSlice.actions;
 export default todosSlice.reducer;
